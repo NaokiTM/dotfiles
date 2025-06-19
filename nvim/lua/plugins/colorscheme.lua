b947916fff612609949542cf4f71ae964f0b3129
Naoki <99505063+NaokiTM@users.noreply.github.com>
2025-06-19 19:01:58 +0100

    Create colorscheme.lua

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+--dracula
+return {
+  -- add dracula
+  { "Mofiqul/dracula.nvim" },
+
+  -- Configure LazyVim to load dracula
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+}
+
+--catpuccin
+-- return {
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "catpuccin",
+--     },
+--   },
+-- }
